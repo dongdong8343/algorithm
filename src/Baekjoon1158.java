36beae39bae036f82f439a66456eb22a816d623d
YuDongGeun <fkdlem8343@naver.com>
2024-03-06 09:03:19 +0900

    1158 요세푸스 문제

---
+++ src/Baekjoon1158.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+public class Baekjoon1158 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        LinkedList<String> list = new LinkedList<>();
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        sb.append('<');
+        for (int i = 1; i <= n; i++) {
+            list.add(String.valueOf(i));
+        }
+        while (!list.isEmpty()) {
+            for (int i = 0; i < k; i++) {
+                if (i == k - 1) {
+                    if (list.size() == 1) {
+                        sb.append(list.remove());
+                    } else {
+                        sb.append(list.remove());
+                        sb.append(", ");
+                    }
+                }else{
+                    list.add(list.remove());
+                }
+            }
+        }
+        sb.append('>');
+        System.out.println(sb);
+    }
+}
