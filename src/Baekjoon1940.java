2cb6492a1523a2a2dcd64ed96fc216da855df9a3
YuDongGeun <fkdlem8343@naver.com>
2024-03-18 10:13:47 +0900

    1940 주몽

--- src/Baekjoon1940.java
+++ src/Baekjoon1940.java
@@ -5,19 +5,17 @@ public class Baekjoon1940 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
-            map.put(i, Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         int cnt = 0;
-        for(int i = 0; i < n; i++){
-            int index = m - map.get(i);
-            if(map.containsValue(index)){
-                cnt++;
+        for(int i = 0; i < n; i ++){
+            for(int j = i+1; j < n; j++){
+                if(arr[i] + arr[j] == m) cnt++;
             }
         }
-        cnt /= 2;
         System.out.println(cnt);
     }
 }
