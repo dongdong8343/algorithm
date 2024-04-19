b60a5a5c5e5120a39992cc70ca743f59081e8a4a
YuDongGeun <fkdlem8343@naver.com>
2024-04-19 11:07:29 +0900

    17298 오큰수 재재도전

--- src/BaekjoonReRe17298.java
+++ src/BaekjoonReRe17298.java
@@ -3,6 +3,7 @@ import java.io.*;
 public class BaekjoonReRe17298 {
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         int arr[] = new int[n];
         int ret[] = new int[n];
@@ -23,6 +24,8 @@ public class BaekjoonReRe17298 {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < n; i++)
             sb.append(ret[i]).append(" ");
-        System.out.println(sb);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
