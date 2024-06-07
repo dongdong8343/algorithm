c587cdd24fb9e7709df879cb02772c247197e843
YuDongGeun <fkdlem8343@naver.com>
2024-06-07 12:11:07 +0900

    1285 숨바꼭질 2 재재재도전

---
+++ src/BaekjoonReReRe1285.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+public class BaekjoonReReRe1285 {
+    static int[] map = new int[24];
+    static int n, ret = 987654321;
+    static void go(int here){
+        if(here == n){
+            int min = 0;
+            for(int i = 1; i < (1 << n); i *= 2){
+                int cnt = 0;
+                for(int j = 0; j < n; j++){
+                    if((map[j] & i) != 0) cnt++;
+                }
+                min += Math.min(cnt, n - cnt);
+            }
+            ret = Math.min(ret, min);
+            return;
+        }
+        go(here + 1);
+        map[here] = ~map[here];
+        go(here + 1);
+    }
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            String s = br.readLine();
+            for(int j = 0; j < n; j++){
+                char c = s.charAt(j);
+                if(c == 'T') map[i] |= (1 << j);
+            }
+        }
+        go(0);
+        System.out.println(ret);
+    }
+}
