b790220de6a643e2d76fa981fdcf4db7389b8ca5
YuDongGeun <fkdlem8343@naver.com>
2024-06-28 11:09:08 +0900

    15684 사다리 조작 오답(6)

---
+++ src/BaekjoonReReReReReRe15684.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+public class BaekjoonReReReReReRe15684 {
+    static int n, h, m, min = 987654321;
+    static int[][] map;
+    static void go(int cnt, int idx){
+        if(cnt > 3 || min <= cnt) return;
+        if(find()){
+            min = Math.min(min, cnt);
+            return;
+        }
+        for(int i = idx; i <= m; i++){
+            for(int j = 1; j < n; j++){
+                if(map[i][j] == 1 || map[i][j - 1] == 1 || map[i][j + 1] == 1) continue;
+                map[i][j] = 1;
+                go(cnt + 1, i);
+                map[i][j] = 0;
+            }
+        }
+    }
+
+    static boolean find(){
+        for(int i = 1; i <= n; i++){
+            int s = i;
+            for(int j = 1; j <= m; j++){
+                if(map[j][s] == 1) s++;
+                else if(map[j][s - 1] == 1) s--;
+            }
+            if(s != i) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[m + 4][n + 4];
+        for(int i = 0; i < h; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            map[a][b] = 1;
+        }
+        go(0, 1);
+        if(min == 987654321) System.out.println(-1);
+        else System.out.println(min);
+    }
+}
