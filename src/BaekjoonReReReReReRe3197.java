b09d5821c4886f1334113e472cba4d4863ac56c9
YuDongGeun <fkdlem8343@naver.com>
2024-06-30 15:31:21 +0900

    3197 백조의 호수

---
+++ src/BaekjoonReReReReReRe3197.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Point;
+public class BaekjoonReReReReReRe3197 {
+    static int r, c;
+    static char[][] map;
+    static int[][] visited;
+    static Point p;
+    static Queue<Point> water = new LinkedList<>();
+    static Queue<Point> swan1 = new LinkedList<>();
+    static Queue<Point> swan2 = new LinkedList<>();
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {-1, 0, 1, 0};
+    static void melt(){
+        int size = water.size();
+        for(int i = 0; i < size; i++){
+            Point temp = water.poll();
+            for(int j = 0; j < 4; j++){
+                int nx = temp.x + dx[j];
+                int ny = temp.y + dy[j];
+                if(ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
+                if(map[ny][nx] == 'X') {
+                    map[ny][nx] = '.';
+                    water.add(new Point(nx, ny));
+                }
+            }
+        }
+    }
+    static boolean move(){
+        while(!swan1.isEmpty()){
+            Point temp = swan1.poll();
+            for(int i = 0; i < 4; i++){
+                int nx = temp.x + dx[i];
+                int ny = temp.y + dy[i];
+                if(ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx] == 1) continue;
+                if(map[ny][nx] == 'L') return true;
+                visited[ny][nx] = 1;
+                if(map[ny][nx] == 'X'){
+                    swan2.add(new Point(nx, ny));
+                    continue;
+                }
+                swan1.add(new Point(nx, ny));
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        map = new char[r + 4][c + 4];
+        visited = new int[r + 4][c + 4];
+        for(int i = 0; i < r; i++){
+            String s = br.readLine();
+            for(int j = 0; j < c; j++){
+                map[i][j] = s.charAt(j);
+                if(map[i][j] == 'L') p = new Point(j, i);
+                if(map[i][j] == 'L' || map[i][j] == '.') water.add(new Point(j, i));
+            }
+        }
+        swan1.add(p);
+        visited[p.y][p.x] = 1;
+
+        int turn = 1;
+        while(true){
+            melt();
+            if(move()) {
+                System.out.println(turn);
+                return;
+            }
+            turn++;
+            swan1 = swan2;
+            swan2 = new LinkedList<>();
+        }
+    }
+}
