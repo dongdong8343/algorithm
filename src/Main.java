d563b007adc60ca015da3bddef6941659a3089a7
YuDongGeun <fkdlem8343@naver.com>
2024-07-02 15:08:15 +0900

    빠르게 코드 작성하기 연습

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,11 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[10];
-
-        while(n != 0){
-            int temp = n % 10;
-            if(temp == 6 || temp == 9){
-                if(arr[6] > arr[9]){
-                    arr[9]++;
-                }
-                else{
-                    arr[6]++;
-                }
-            }
-            else{
-                arr[temp] += 1;
-            }
-            n /= 10;
-        }
-
-        int max = arr[0];
-        for(int i = 1; i<10; i++){
-            if(max < arr[i]) {
-                max = arr[i];
-            }
-        }
-
-        System.out.println(max);
-
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.println((double) n / m);
     }
 }
