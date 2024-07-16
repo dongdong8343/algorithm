63077d4d24ff05bb6c5da6d0a4313e7435499fa9
YuDongGeun <fkdlem8343@naver.com>
2024-07-16 12:13:34 +0900

    1700 멀티탭 스케줄링 오답(3)

---
+++ src/BaekjoonRe1700_3.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+public class BaekjoonRe1700_3 {
+    static int n, k, ret = 0;
+    static int[] a = new int[104];
+    static int[] v = new int[104];
+    static List<Integer> li = new ArrayList<>();
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < k; i++){
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i < k; i++){
+            if(v[a[i]] == 0){
+                if(li.size() == n){
+                    int last = 0;
+                    int pos = -1;
+                    for(int x : li){
+                        int idx = 987654321;
+                        for(int j = i + 1; j < k; j++){
+                            if(x == a[j]){
+                                idx = j;
+                                break;
+                            }
+                        }
+                        if(idx > last){
+                            last = idx;
+                            pos = x;
+                        }
+                    }
+                    v[pos]--;
+                    ret++;
+                    li.remove((Integer) pos);
+                }
+                v[a[i]]++;
+                li.add(a[i]);
+            }
+        }
+        System.out.println(ret);
+    }
+}
