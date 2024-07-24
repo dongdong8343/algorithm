c79c4d6db7ba38cbb2a6eb8e429da59bfc7b375e
YuDongGeun <fkdlem8343@naver.com>
2024-07-24 11:36:53 +0900

    1912 연속합 오답(1)

---
+++ src/BaekjoonRe1912_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class BaekjoonRe1912_1 {
+    static int n, sum = 0, ret = -987654321;
+    public static void main(String[] args)throws Exception{
+        BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            int x = Integer.parseInt(st.nextToken());
+            sum += x;
+            ret = Math.max(ret, sum);
+            if(sum < 0) sum = 0;
+        }
+        System.out.println(ret);
+    }
+}
