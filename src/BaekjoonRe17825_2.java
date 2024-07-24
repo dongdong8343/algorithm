e505e9d70448fb6a3abc6b11fb0a7fafedde8908
YuDongGeun <fkdlem8343@naver.com>
2024-07-24 11:37:28 +0900

    17825 주사위 윷놀이 오답(2)

--- src/BaekjoonRe17825_2.java
+++ src/BaekjoonRe17825_2.java
@@ -1,72 +1,78 @@
 import java.util.*;
 import java.io.*;
 public class BaekjoonRe17825_2 {
-    static List<Integer>[] adj = new List[54];
-    static int[] v = new int[104];
-    static int[] a = new int[12];
-    static int[] mal = new int[4];
-    static int go(int here){
-        if(here == 10) return 0;
-        int ret = 0;
-        for(int i = 0; i < 4; i++){
-            int t = mal[i];
-            int n = move(t, a[here]);
-            if(ismal(n, i)) continue;
-            mal[i] = n;
-            ret = Math.max(ret, go(here + 1) + v[n]);
-            mal[i] = t;
+    static int ret = 0;
+    static int[] a = new int[14];
+    static List<Integer>[] map = new List[54];
+    static int[] sc = new int[104];
+    static void setMap(){
+        for(int i = 0; i < 54; i++)
+            map[i] = new ArrayList<>();
+
+        for(int i = 0; i < 20; i++){
+            map[i].add(i + 1);
         }
-        return ret;
+        map[5].add(21); map[21].add(22); map[22].add(23); map[23].add(24);
+        map[15].add(27); map[27].add(26); map[26].add(25); map[25].add(24);
+        map[10].add(28); map[28].add(29); map[29].add(24); map[24].add(30);
+        map[30].add(31); map[31].add(20); map[20].add(100);
+
+        for(int i = 1; i < 21; i++){
+            sc[i] = i * 2;
+        }
+        sc[21] = 13; sc[22] = 16; sc[23] = 19; sc[24] = 25;
+        sc[25] = 26; sc[26] = 27; sc[27] = 28; sc[28] = 22;
+        sc[29] = 24; sc[30] = 30; sc[31] = 35;
     }
-    static int move(int now, int cnt){
+    static int[] mal = new int[4];
+    static int move(int now, int num){
         if(now == 100) return 100;
-        if(adj[now].size() >= 2){
-            now = adj[now].get(1);
-            cnt--;
+        if(map[now].size() == 2){
+            now = map[now].get(1);
+            num--;
         }
-        if(cnt > 0){
-            Queue<Integer> q = new LinkedList<>();
-            int there = 0;
-            q.add(now);
-            while(!q.isEmpty()){
-                int x = q.poll();
-                there = adj[x].get(0);
-                q.add(there);
-                if(there == 100) break;
-                cnt--;
-                if(cnt == 0) break;
+        int next;
+        if(num > 0){
+            while(true){
+                next = map[now].get(0);
+                num--;
+                if(num == 0) break;
+                if(next == 100) break;
+                now = next;
             }
-            return there;
+            return next;
         }else return now;
     }
-    static boolean ismal(int now, int idx){
-        if(now == 100) return false;
+    static boolean ismal(int h, int n){
+        if(h == 100) return false;
         for(int i = 0; i < 4; i++){
-            if(i == idx) continue;
-            if(now == mal[i]) return true;
+            if(i == n) continue;
+            if(mal[i] == h) return true;
         }
         return false;
     }
-    static void setMap(){
-        for(int i = 0; i < 54; i++) adj[i] = new ArrayList<>();
-        for(int i = 0; i <= 19; i++) adj[i].add(i + 1);
-        adj[5].add(21); adj[21].add(22); adj[22].add(23); adj[23].add(24);
-        adj[10].add(27); adj[27].add(28); adj[28].add(24); adj[15].add(29);
-        adj[29].add(30); adj[30].add(31); adj[31].add(24); adj[24].add(25);
-        adj[25].add(26); adj[26].add(20); adj[20].add(100);
-
-        v[1] = 2; v[2] = 4; v[3] = 6; v[4] = 8; v[5] = 10;
-        v[6] = 12; v[7] = 14; v[8] = 16; v[9] = 18; v[10] = 20;
-        v[11] = 22; v[12] = 24; v[13] = 26; v[14] = 28; v[15] = 30;
-        v[16] = 32; v[17] = 34; v[18] = 36; v[19] = 38; v[20] = 40;
-        v[21] = 13; v[22] = 16; v[23] = 19; v[24] = 25; v[25] = 30;
-        v[26] = 35; v[27] = 22; v[28] = 24; v[29] = 28; v[30] = 27; v[31] = 26;
+    static void go(int here, int score){
+        if(here == 10){
+            ret = Math.max(score, ret);
+            return;
+        }
+        for(int i = 0; i < 4; i++){
+            int now = mal[i];
+            int next = move(now, a[here]);
+            if(ismal(next, i)) continue;
+            mal[i] = next;
+            go(here + 1, score + sc[next]);
+            mal[i] = now;
+        }
     }
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         setMap();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 10; i++) a[i] = Integer.parseInt(st.nextToken());
-        System.out.println(go(0));
+        for(int i = 0; i < 10; i++){
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        go(0, 0);
+        System.out.println(ret);
     }
 }
