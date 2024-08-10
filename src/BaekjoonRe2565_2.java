3f96c73a485a8e161446b0587eaccbc1b0697a7b
YuDongGeun <fkdlem8343@naver.com>
2024-08-10 12:13:32 +0900

    2565 전깃줄 오답(3)

---
+++ src/BaekjoonRe2565_2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Point;
+public class BaekjoonRe2565_2 {
+    static int n, l, h;
+    static List<Point> li = new ArrayList<>();
+    static List<Integer> a = new ArrayList<>();
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            li.add(new Point(x, y));
+        }
+        Collections.sort(li, (o1, o2) -> o1.x - o2.x);
+        a.add(li.get(0).y);
+        for(int i = 1; i < li.size(); i++){
+            if(li.get(i).y > a.get(a.size() - 1)) a.add(li.get(i).y);
+            else{
+                l = 0; h = a.size() - 1;
+                while(l < h){
+                    int mid = (l + h) / 2;
+                    if(a.get(mid) >= li.get(i).y) h = mid;
+                    else l = mid + 1;
+                }
+                a.set(h, li.get(i).y);
+            }
+        }
+        System.out.println(n - a.size());
+    }
+}
--- src/BaekjoonRe2565_3.java
+++ src/BaekjoonRe2565_3.java
@@ -3,31 +3,35 @@ import java.io.*;
 import java.awt.Point;
 public class BaekjoonRe2565_3 {
     static int n, l, h;
-    static List<Point> li = new ArrayList<>();
-    static List<Integer> a = new ArrayList<>();
+    static List<Point> arr = new ArrayList<>();
+    static List<Integer> li = new ArrayList<>();
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         for(int i = 0; i < n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            li.add(new Point(x, y));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr.add(new Point(a, b));
         }
-        Collections.sort(li, (o1, o2) -> o1.x - o2.x);
-        a.add(li.get(0).y);
-        for(int i = 1; i < li.size(); i++){
-            if(li.get(i).y > a.get(a.size() - 1)) a.add(li.get(i).y);
+        Collections.sort(arr, (o1, o2) -> {
+            return o1.x - o2.x;
+        });
+        li.add(arr.get(0).y);
+        for(int i = 1; i < n; i++){
+            int x = arr.get(i).y;
+            if(li.get(li.size() - 1) < x) li.add(x);
             else{
-                l = 0; h = a.size() - 1;
+                l = 0; h = li.size() - 1;
                 while(l < h){
                     int mid = (l + h) / 2;
-                    if(a.get(mid) >= li.get(i).y) h = mid;
-                    else l = mid + 1;
+                    if(li.get(mid) >= x) {
+                        h = mid;
+                    }else l = mid + 1;
                 }
-                a.set(h, li.get(i).y);
+                li.set(h, x);
             }
         }
-        System.out.println(n - a.size());
+        System.out.println(n - li.size());
     }
 }
