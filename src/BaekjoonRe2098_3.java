9a0960a4f726b051d4d665bea69d31331a5b955b
YuDongGeun <fkdlem8343@naver.com>
2024-08-14 09:49:25 +0900

    2098 외판원 순회 오답(3)

---
+++ src/BaekjoonRe2098_3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+public class BaekjoonRe2098_3 {
+    static int n;
+    static int[][] w = new int[20][20];
+    static int[][] dp = new int[16][(1 << 16)];
+    static int go(int here, int v){
+        if((v & (1 << n) - 1) == (1 << n) - 1){
+            return w[here][0] != 0 ? w[here][0] : 987654321;
+        }
+
+        if(dp[here][v] != -1) return dp[here][v];
+
+        dp[here][v] = 987654321;
+        for(int i = 0; i < n; i++){
+            if((v & (1 << i)) != 0) continue;
+            if(w[here][i] == 0) continue;
+            dp[here][v] = Math.min(dp[here][v], go(i, v | (1 << i)) + w[here][i]);
+        }
+        return dp[here][v];
+    }
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                w[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int[] row : dp) Arrays.fill(row, -1);
+        System.out.println(go(0, 1));
+    }
+}
