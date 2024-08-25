d3619d6758be7e98a4918f82ee7f85585bb82664
YuDongGeun <fkdlem8343@naver.com>
2024-08-25 19:51:30 +0900

    2792 보석 상자 오답(4)

---
+++ src/BaekjoonRe2792_4.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+public class BaekjoonRe2792_4 {
+    static int n, m;
+    static long l, h, ret = 0;
+    static int[] a = new int[300004];
+    static boolean check(long mid){
+        long cnt = 0;
+        for(int i = 0; i < m; i++){
+            cnt += a[i] / mid;
+            if(a[i] % mid != 0) cnt++;
+        }
+        return cnt <= n;
+    }
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < m; i++) a[i] = Integer.parseInt(br.readLine());
+        l = 1; h = (long)1e9;
+        while(l <= h){
+            long mid = (l + h) / 2;
+            if(check(mid)){
+                h = mid - 1;
+                ret = mid;
+            }else l = mid + 1;
+        }
+        System.out.println(ret);
+    }
+}
