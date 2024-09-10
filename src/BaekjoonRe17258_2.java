7186af02a04d43d4b59978384ce880f22d14ff89
YuDongGeun <fkdlem8343@naver.com>
2024-09-10 11:46:47 +0900

    17258 인기가 넘쳐흘러 오답(2)

---
+++ src/BaekjoonRe17258_2.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Point;
+public class BaekjoonRe17258_2 {
+    static int n, m, k, t, s, e;
+    static int[] a = new int[304];
+    static int[][] dp = new int[304][304];
+    static List<Point> v = new ArrayList<>();
+    // num = 얼마나 투입 가능한지
+    // prev = 이전에 얼마나 썼는지
+    static int go(int here, int num, int prev){
+        if(here == v.size()) return 0;
+        if(dp[here][num] != 0) return dp[here][num];
+        // 필요한 cost
+        int cost = Math.max(0, t - v.get(here).y);
+        int real_cost = (prev >= cost) ? 0 : cost - prev;
+        if(num - real_cost >= 0)
+            return dp[here][num] = Math.max(go(here + 1, num - real_cost, cost) + v.get(here).x, go(here + 1, num, 0));
+        else return dp[here][num] = go(here + 1, num, 0);
+    }
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            s = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
+            for(int j = s; j < e; j++){
+                a[j] = Math.min(t, ++a[j]);
+            }
+        }
+        int temp = a[1];
+        int cnt = 1;
+        for(int i = 2; i <= n; i++){
+            if(temp != a[i]){
+                v.add(new Point(cnt, temp));
+                cnt = 0;
+                temp = a[i];
+            }
+            cnt++;
+        }
+        v.add(new Point(cnt, temp));
+        System.out.println(go(0, k, 0));
+    }
+}
