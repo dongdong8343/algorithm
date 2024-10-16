72e69410836b7bcd44d02029af5bcafd6a9411cf
YuDongGeun <fkdlem8343@naver.com>
2024-10-16 09:14:43 +0900

    프로그래머스 주식가격

--- src/programmers42584.java
+++ src/programmers42584.java
@@ -6,6 +6,7 @@ class programmers42584 {
         Stack<Integer> st = new Stack<>();
 
         for(int i = 0; i < p.length; i++){
+            // 스택이 비어있지않고 현재 주식 가격이 스택 맨 위의 값보다 떨어진 경우 스택을 pop해서 기간을 배열에 기록함.
             while(!st.isEmpty() && p[st.peek()] > p[i]){
                 int idx = st.pop();
                 ret[idx] = i - idx;
@@ -13,6 +14,7 @@ class programmers42584 {
             st.push(i);
         }
 
+        // 스택에 값들이 남아있다면 돌면서 기간을 배열에 기록함.
         while(!st.isEmpty()){
             int idx = st.pop();
             ret[idx] = p.length - idx - 1;
