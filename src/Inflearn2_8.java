0ba2f04597ddd386634d0540628ffff143b67573
YuDongGeun <fkdlem8343@naver.com>
2024-11-16 12:00:55 +0900

    코딩테스트 강의 2-8 등수 구하기 (다시 풀었음)

--- src/Inflearn2_8.java
+++ src/Inflearn2_8.java
@@ -2,38 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Inflearn2_8 {
-    static int n, num = 0, last = 0, cnt = 1;
-    static List<Integer> li = new ArrayList<>();
-    static int[][] a = new int[2][104];
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[104];
+        int[] ret = new int[104];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            a[i] = Integer.parseInt(st.nextToken());
+        }
 
         for(int i = 0; i < n; i++){
-            a[0][i] = Integer.parseInt(st.nextToken());
-            li.add(a[0][i]);
-        }
-
-        Collections.sort(li, Collections.reverseOrder());
-
-        for(int x : li){
-            if(last == x) {
-                cnt++;
-                continue;
+            int cnt = 1;
+            for(int j = 0; j < n; j++){
+                if(a[j] > a[i]) cnt++;
             }
-            num += cnt;
-            cnt = 1;
-            for(int i = 0; i < n; i++){
-                if(a[0][i] == x) a[1][i] = num;
-            }
-            last = x;
+            ret[i] = cnt;
         }
 
-
-        for(int i = 0; i < n; i++){
-            System.out.print(a[1][i] + " ");
-        }
+        for(int i = 0; i < n; i++) System.out.print(ret[i] + " ");
 
         return;
     }
