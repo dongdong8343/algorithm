563dcb97a24f7c7d16529baf2c809e59baefd2e1
dongdong8343 <fkdlem8343@naver.com>
2025-01-31 16:44:18 +0900

    백준15686 치킨 배달

--- src/BaekjoonRe15686.java
+++ src/BaekjoonRe15686.java
@@ -8,43 +8,43 @@ public class BaekjoonRe15686 {
     static List<Point> chickens = new ArrayList<>();
     static List<Point> v = new ArrayList<>();
 
-    static void go(int s){
-        if(v.size() == m){
+    static void go(int s) {
+        if (v.size() == m) {
             cal();
             return;
         }
-        for(int i = s + 1; i < cnt; i++){
+        for (int i = s + 1; i < cnt; i++) {
             v.add(chickens.get(i));
             go(i);
             v.remove(chickens.get(i));
         }
     }
 
-    static void cal(){
+    static void cal() {
         int sum = 0;
-        for(Point h : houses){
+        for (Point h : houses) {
             int dist = 987654321;
-            for(Point c : v){
+            for (Point c : v) {
                 dist = Math.min(dist, Math.abs(c.y - h.y) + Math.abs(c.x - h.x));
             }
             sum += dist;
-            if(ret < sum) return;
+            if (ret < sum) return;
         }
         ret = Math.min(ret, sum);
     }
 
-    public static void main(String[] args)throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
 
         n = Integer.parseInt(s[0]);
         m = Integer.parseInt(s[1]);
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             s = br.readLine().split(" ");
-            for(int j = 0; j < n; j++){
-                if(Integer.parseInt(s[j]) == 1) houses.add(new Point(j, i));
-                else if(Integer.parseInt(s[j]) == 2) {
+            for (int j = 0; j < n; j++) {
+                if (Integer.parseInt(s[j]) == 1) houses.add(new Point(j, i));
+                else if (Integer.parseInt(s[j]) == 2) {
                     chickens.add(new Point(j, i));
                     cnt++;
                 }
