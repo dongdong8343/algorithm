dcda3464a72ff2c4d3e345638ae20713560a07d9
dongdong8343 <fkdlem8343@naver.com>
2025-02-04 16:40:55 +0900

    백준 1016 (미해결)

---
+++ src/Baekjoon1016.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon1016 {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
