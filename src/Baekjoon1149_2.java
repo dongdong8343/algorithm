4ddd38c050973cfa354a31ff013305dec74f1105
dongdong8343 <fkdlem8343@naver.com>
2025-02-09 14:40:23 +0900

    백준1149 RGB 거리

--- src/Baekjoon1149_2.java
+++ src/Baekjoon1149_2.java
@@ -2,38 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Baekjoon1149_2 {
+    static final int R = 0, G = 1, B = 2;
     static int ret = 987654321, n;
     static int[][] a = new int[1004][10];
     static int[][] dp = new int[1004][10];
 
-    public static void main(String[] args)throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             String[] st = br.readLine().split(" ");
-            Arrays.fill(dp[i], 987654321);
-            for(int j = 0; j < 3; j++){
+            for (int j = 0; j < 3; j++) {
                 a[i][j] = Integer.parseInt(st[j]);
             }
         }
 
-        for(int i = 0; i < 3; i++){
-            ret = Math.min(ret, go(0, i));
+        dp[0][R] = a[0][R];
+        dp[0][G] = a[0][G];
+        dp[0][B] = a[0][B];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][R] = Math.min(a[i][R] + dp[i - 1][G], a[i][R] + dp[i - 1][B]);
+            dp[i][G] = Math.min(a[i][G] + dp[i - 1][R], a[i][G] + dp[i - 1][B]);
+            dp[i][B] = Math.min(a[i][B] + dp[i - 1][R], a[i][B] + dp[i - 1][G]);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if(ret > dp[n - 1][i]) ret = dp[n - 1][i];
         }
 
         System.out.println(ret);
-    }
-
-    static int go(int depth, int before){
-        if(depth == n) return 0;
-        if(dp[depth][before] != 987654321) return dp[depth][before];
-
-        for(int i = 0; i < 3; i++){
-            if(before == i) continue;
-            dp[depth][before] = Math.min(dp[depth][before], a[depth][i] + go(depth + 1, i));
-        }
-
-        return dp[depth][before];
     }
 }
