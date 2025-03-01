bfb6f5158d9cd94a1949c5fdf5e552542e27cdad
dongdong8343 <fkdlem8343@naver.com>
2025-03-01 16:03:43 +0900

    백준 11286 절댓값 힙

---
+++ src/Baekjoon12286.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon12286 {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> q = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(Math.abs(o1) == Math.abs(o2)) return o1 - o2;
+                return Math.abs(o1) - Math.abs(o2);
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            int temp = Integer.parseInt(br.readLine());
+            if(temp == 0){
+                if(q.isEmpty()) sb.append(0 + "\n");
+                else sb.append(q.poll()).append("\n");
+            } else q.add(temp);
+        }
+
+        System.out.println(sb.toString());
+
+
+    }
+
+}
