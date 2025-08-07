6bf4419400f7ddf034c9dd511cfbc8ebef98a58f
dongdong8343 <fkdlem8343@naver.com>
2025-08-07 10:50:47 +0900

    인프런 - 자바 알고리즘 1-12

---
+++ src/I_12.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class I_12 {
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+
+		for(int i = 0; i < n; i++) {
+			String tmp = s.substring(i * 7, i * 7 + 7);
+			String bi = tmp.replace('#', '1').replace('*', '0');
+
+			int num = Integer.parseInt(bi, 2);
+			System.out.printf("%c", num);
+		}
+	}
+}
